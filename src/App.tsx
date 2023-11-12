d82b88983bebf930eb00a13995956ab091a51942
Sebastian Niguleasa <seba80443@gmail.com>
2023-11-12 17:55:10 +0200

    Add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { Button } from 'shadcn/ui/button';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from 'shadcn/ui/tabs';
+
 export function App() {
-    return <div className="text-3xl">TEST</div>;
+    return (
+        <div className="text-3xl">
+            <Button>Aaaa test</Button>
+
+            <Tabs defaultValue="account" className="w-[400px]">
+                <TabsList>
+                    <TabsTrigger value="account">Account</TabsTrigger>
+                    <TabsTrigger value="password">Password</TabsTrigger>
+                </TabsList>
+                <TabsContent value="account">Make changes to your account here.</TabsContent>
+                <TabsContent value="password">Change your password here.</TabsContent>
+            </Tabs>
+        </div>
+    );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     resolve: {
         alias: {
+            shadcn: '/src/shadcn',
             components: '/src/components',
             hooks: '/src/hooks'
         }
