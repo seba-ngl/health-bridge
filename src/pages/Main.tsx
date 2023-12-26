8e890e6c9d573a0c13ff6f84529b5a6d612e5455
sniguleasa <seba80443@gmail.com>
2023-12-26 22:27:44 +0200

    Remove test button

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,14 +2,12 @@ import { useTranslation } from 'react-i18next';
 
 import { LanguageSelect } from 'components/LanguageSelect';
 import { ThemeButton } from 'components/ThemeButton';
-import { Button } from 'shadcn/ui/button';
 
 export function Main() {
     const { t } = useTranslation();
 
     return (
         <div className="flex gap-4">
-            <Button>{t('web.test')}</Button>
             <LanguageSelect />
             <ThemeButton />
         </div>
